e9052cb67ee0a1af10f4acaacf8b7e7392848428
Stepa-Kuropatkin <igortrotsuk@gmail.com>
2021-01-13 00:46:13 +0300

    homepage def

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,21 +1,124 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weatherapp/models/forecast_model.dart';
+import 'package:weatherapp/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:weatherapp/widgets/weather.dart';
+import 'package:weatherapp/widgets/weather_item.dart';
 
 class Homescreen extends StatefulWidget {
-  Homescreen({Key key}) : super(key: key);
-
   @override
   _HomescreenState createState() => _HomescreenState();
 }
 
 class _HomescreenState extends State<Homescreen> {
+  bool isLoading = false;
+  WeatherData weatherData;
+  ForecastData forecastData;
+
+  loadWeather() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    Position position;
+    try {
+      position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+    } catch (e) {
+      print(e);
+    }
+
+    if (position != null) {
+      final lat = position.latitude;
+      final lon = position.longitude;
+
+      final weatherResponse = await http.get(
+          'https://api.openweathermap.org/data/2.5/weather?lat=${lat.toString()}&lon=${lon.toString()}&lang=ru&appid=93f5125b22308c4d1a74be841366d998');
+      final forecastResponse = await http.get(
+          'https://api.openweathermap.org/data/2.5/forecast?lat=${lat.toString()}&lon=${lon.toString()}&lang=ru&appid=93f5125b22308c4d1a74be841366d998');
+
+      if (weatherResponse.statusCode == 200 &&
+          forecastResponse.statusCode == 200) {
+        return setState(() {
+          weatherData =
+              new WeatherData.fromJson(jsonDecode(weatherResponse.body));
+          forecastData =
+              new ForecastData.fromJson(jsonDecode(forecastResponse.body));
+          isLoading = false;
+        });
+      }
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Weather'),
+        title: Text('Current Weather'),
       ),
       body: Center(
-        child: Text('Привет, калымаги!'),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: weatherData != null
+                        ? Weather(weather: weatherData)
+                        : Text('Sorry, no data'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: isLoading
+                        ? CircularProgressIndicator(
+                            strokeWidth: 2.0,
+                            valueColor: AlwaysStoppedAnimation(Colors.white),
+                          )
+                        : IconButton(
+                            icon: Icon(Icons.refresh),
+                            tooltip: 'Refresh',
+                            onPressed: () => loadWeather(),
+                            color: Colors.white,
+                          ),
+                  ),
+                ],
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  height: 200.0,
+                  child: forecastData != null
+                      ? ListView.builder(
+                          itemCount: forecastData.list.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) => WeatherItem(
+                            weather: forecastData.list.elementAt(index),
+                          ),
+                        )
+                      : Text('Sorry, no Data'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
